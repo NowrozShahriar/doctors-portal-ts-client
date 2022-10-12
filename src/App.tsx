8348eed640992aa0932045f84142d121ca77d421
nowrozshahriar <nowroz.shahriar@yahoo.com>
2022-10-12 23:30:14 +0600

    homepage banner added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Pages/Shared/Navbar';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
